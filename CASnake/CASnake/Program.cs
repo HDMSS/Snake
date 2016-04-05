21dc15489889e63abcc58837a7b1a8e015881767
HDMSS <morozov.serhii.pl@gmai.com>
2016-04-05 15:30:04 +0200

    Lesson_6

--- CASnake/CASnake/Program.cs
+++ CASnake/CASnake/Program.cs
@@ -10,14 +10,37 @@ namespace CASnake
     {
         static void Main(string[] args)
         {
-            Point p3 = new Point(1, 5, '$');
-            p3.Draw();
-            Point p1 = new Point(2, 2, '@');
+            Point p1 = new Point(1, 1, '$');
             p1.Draw();
-            Point p2 = new Point(3, 3, '#');
+            Point p2 = new Point(2, 2, '@');
             p2.Draw();
+            Point p3 = new Point(3, 3, '#');
+            p3.Draw();
+            Point p4 = new Point(4, 4, '&');
+            p4.Draw();
 
-           
+            List<int> numList = new List<int>();
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(3);
+            numList.Add(4);
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            int s = numList[3];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.RemoveAt(0);
+
+            List<Point> pointlList = new List<Point>();
+            pointlList.Add(p1);
+            pointlList.Add(p2);
+            pointlList.Add(p3);
+            pointlList.Add(p4);
+
             Console.ReadLine();
         }
 
